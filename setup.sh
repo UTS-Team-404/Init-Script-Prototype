25baf17036ebcf28fd5fefce905bdaf915783359
Rennecfox <154237006+Rennecfox@users.noreply.github.com>
2025-10-16 15:22:41 +1100

    Now initialises all other scripts dependencies

--- setup.sh
+++ setup.sh
@@ -1,8 +1,34 @@
 #! /bin/bash
 #
-#	Wifi Tool init script
-#	v2.0 - 14/10/2025
-#	Author Aaron - Team 404
+#         WiFinder
+#	"Wifi Tool init script"
+#	  v3.0 - 16/10/2025
+# Authors Aaron, Harry - Team 404
+#
+#   First run this to configure
+#    any new hardware easily.
+#
+
+REQUIREMENTS_FILE="requirements.txt"
+VENV_DIR=".venv"
+LOGFILE="./initLogs/$(date +'%Y-%m-%d_%H:%M:%S').log"
+
+								#Set target file
+#Might not support paths beyond root directory currently (Need better pattern matching)
+absolutetarget="/etc/rc.local"
+echo "Target is set to $absolutetarget"
+targetfile=$(echo $absolutetarget | awk 'BEGIN { FS = "/" } ; { print $NF }')
+targetdir=$(echo $absolutetarget | awk 'BEGIN {FS = "/"} ; {$NF--;print}')
+
+###################################################################################
+                                #Functions
+#clear or make logfile
+: > "$LOGFILE"
+
+#Function to log both to file and stdout (yet to be fully implemented in here)
+log() {
+    echo -e "$@" | tee -a "$LOGFILE"
+}
 
 #Yes or No Function
 yes() {
@@ -17,67 +43,300 @@ yes() {
   done
 }
 
+###################################################################################
+                                #Start
+log "\n===== Initialization started at $(date) =====\n"
+								#Banner and instructions
+echo "###################################"
+echo "########## WiFinder Init ##########"
+echo "###################################"
+echo ""
+echo "This file must be run as root and should really only be run once.
+If you need to rerun for whatever reason, first run the init-wiper.sh script first and then rerun this. Also should probably update before running this. Python3 v3.8 required."
 
+echo "Variables set: (default [/etc/rc.local] [etc] [rc.local] )"
+echo $absolutetarget
+echo $targetfile
+echo $targetdir
+echo "Run? (y/n/c)"
+if yes(); then
+    echo "running..."
+else
+    echo "exiting..."
+    exit
+fi
+
+#Check for root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script needs to be run with sudo or as root."
+    exit 1
+fi
+echo "as root..."
+
+###################################################################################
+                                #FIRST SETUP
 
 								#Set Touchscreen and Rotation
 #This is for the screen specified in our hardware outline. Other screens can be used, please change as required. screenrotateset is a temperary status file and not required.
+echo ""
+echo "###################################"
+echo "###### LCD Touchscreen setup ######"
+echo "###################################"
+
 if [ ! -f /home/LCD-show/ ]; then
     echo "LCD-show not found, cloning... Then installing and restarting"
-    git clone https://github.com/goodtft/LCD-show.git
-    sudo chmod +x /home/LCD-show/*
-    sudo /home/LCD-show/LCD5-show
+    sudo git clone -c /$targetdir https://github.com/goodtft/LCD-show.git
+    sudo chmod +x $targetdir/LCD-show/*
+    sudo /$targetdir/LCD-show/LCD5-show
     exit
     else
-    if [ ! -f /home/LCD-show/screenrotateset ]; then
+    if [ ! -f /$targetdir/LCD-show/screenrotateset ]; then
         echo "LCD-show installed but rotation isnt set, installing and restarting"
-        touch /home/LCD-show/screenrotateset
-        sudo /home/LCD-show/rotate.sh 270
+        touch /$targetdir/LCD-show/screenrotateset
+        sudo /$targetdir/LCD-show/rotate.sh 270
         exit
-    else
+        else
         echo "LCD-show found, installed and rotation set, continue..."
     fi
 fi
 
-
 								#Check rc.local
 #/etc/rc.local is now depricated and can either be enabled in systemd, or can instead be run as a service which should be what this is upgraded to in the future.
+echo "###################################"
+echo "##### rc.local service check ######"
+echo "###################################"
 
-if [ ! -f /ect/rc.local ]; then
-    echo "rc.local file not found. Check if rc-local.service is enabled:"
-    sudo systemctl is-enabled rc-local.service
-    sudo systemctl status rc-local.service
-    echo "Does rc.local need to be enabled/reset?"    
-    if yes(); then
-        sudo touch /etc/rc.local
-        sudo sh -c 'echo "#!/bin/bash" > /etc/rc.local'
-        sudo sh -c 'echo "sleep 1" >> /etc/rc.local'
-        sudo chmod +x /etc/rc.local
-        sudo systemctl enable rc-local.service
-    else
-        echo "try checking the file exists or checking its enabled/executable"
-        exit
-    fi  
+echo "Are you using rc.local as autostart service? (y/n/c) (default yes)"
+if yes(); then
+    if [ ! -f /ect/rc.local ]; then
+        echo "rc.local file not found. Check if rc-local.service is enabled:"
+        sudo systemctl is-enabled rc-local.service
+        sudo systemctl status rc-local.service
+        echo "Does rc.local need to be enabled/reset? (y/n/c)"    
+        if yes(); then
+            sudo touch /etc/rc.local
+            sudo sh -c 'echo "#!/bin/bash" > /etc/rc.local'
+            sudo sh -c 'echo "sleep 1" >> /etc/rc.local'
+            sudo chmod +x /etc/rc.local
+            sudo systemctl enable rc-local.service
+        else
+            echo "try checking the file exists or checking its enabled/executable"
+            echo "exiting..."
+            exit
+        fi
+    fi
+else
+    echo "Skipping rc.local check. ensure that you have changed the absolute target in this script to the desired output file."
+    echo "Press any key to confirm it has been changed and continue."
+    read -s -n 1
 fi
 
 
-								#Set target file
-#Might not support paths beyond root directory currently (Need better pattern matching)
-absolutetarget="/etc/rc.local"
-targetfile=$(echo $absolutetarget | awk 'BEGIN { FS = "/" } ; { print $NF }')
-targetdir=$(echo $absolutetarget | awk 'BEGIN {FS = "/"} ; {$NF--;print}')
-echo $absolutetarget
-echo $targetfile
-echo $targetdir
 
-	
-								#Banner and instructions
+
+###################################################################################
+                                #Dependancies  - by Harry
+
+								#Check Install dependencies
+echo ""
 echo "###################################"
-echo "#Team 404 Wifi Analitics tool Init#"
+echo "######## Main Dependancies ########"
 echo "###################################"
 echo ""
-echo "This file must be run as root and must be only run once.
-If you need to rerun for whatever reason, first run the init-wiper.sh script first and then rerun this."
+APT_PACKAGES=(
+    python3-gi
+    python3-gi-cairo
+    gir1.2-gtk-3.0
+    libgirepository1.0-dev
+    libcairo2-dev
+    pkg-config
+    python3-dev
+    libgtk-3-dev
+    build-essential
+    libwebkit2gtk-4.1-dev
+    gir1.2-webkit2-4.1
+    libopenblas-dev
+    mysql-server
+)
+
+echo "\nInstalling required system packages..."
+apt install -y "${APT_PACKAGES[@]}" 2>&1 | tee -a "$LOGFILE"
+if [ $? -eq 0 ]; then
+    log "System packages installed successfully."
+else
+    log "some packages failed to install."
+fi
+
+                                #PythonSetup
 echo ""
+echo "###################################"
+echo "######## Python Venv setup ########"
+echo "###################################"
+echo ""
+echo -e "This will check for python version and then setup the virtual environment\n"
+
+# ---Check for Python 3 ---
+if command -v python3 &>/dev/null; then
+    PY_VERSION=$(python3 -V 2>&1)
+    log "[+] Python found: $PY_VERSION"
+else
+    log "[!] Python3 is not installed. Please install it and re-run this script."
+    exit 1
+fi
+
+# --- Check Python version compatibility ---
+REQUIRED_VERSION="3.8"
+if python3 -c "import sys; exit(0 if sys.version_info >= (3,8) else 1)"; then
+    log "[+] Python version >= $REQUIRED_VERSION OK"
+else
+    log "[!] Python version too old. Please upgrade to $REQUIRED_VERSION+"
+    exit 1
+fi
+
+# --- Check for venv module ---
+if ! python3 -m venv --help &>/dev/null; then
+    log "[!] The 'venv' module is not available in your Python installation."
+    exit 1
+fi
+
+# --- Create virtual environment if missing ---
+if [ ! -d "$VENV_DIR" ]; then
+    log "[*] Creating Python virtual environment in $VENV_DIR ..."
+    python3 -m venv "$VENV_DIR"
+    if [ $? -ne 0 ]; then
+        log "[X] Failed to create virtual environment."
+        exit 1
+    fi
+else
+    log "[+] Virtual environment already exists ($VENV_DIR)"
+fi
+
+# --- Activate the venv ---
+log "[*] Activating virtual environment..."
+# shellcheck disable=SC1091
+source "$VENV_DIR/bin/activate"
+
+# --- Upgrade pip ---
+log "[*] Upgrading pip inside venv..."
+pip install --upgrade pip 2>&1 | tee -a "$LOGFILE"
+
+# --- Install requirements ---
+if [ -f "$REQUIREMENTS_FILE" ]; then
+    log "[*] Installing Python dependencies from $REQUIREMENTS_FILE ..."
+    pip install -r "$REQUIREMENTS_FILE" 2>&1 | tee -a "$LOGFILE"
+else
+    log "[!] No requirements.txt found — skipping dependency check."
+fi
+
+
+                                #GPS Setup
+echo ""
+echo "###################################"
+echo "######## GPS reader Setup #########"
+echo "###################################"
+echo ""
+sudo apt install -y aircrack-ng gpsd gpsd-clients python3-gps python3-scapy
+
+#GPS SETUP HERE?
+
+sudo systemctl stop gpsd
+sudo systemctl stop gpsd.socket
+echo "gpsd set udp://localhost:7777"
+gpsd -N udp://localhost:7777
+echo "update /etc/default/gpsd to make perminent"
+# use sed to change the config. add -i to actually write to the file
+#sudo  sed -i 's/TEXTMATCHINGTHIS/REPLACEDWITHTHIS/' /etc/default/gpsd
+
+sudo systemctl start gpsd
+sudo systemctl start gpsd.socket
+echo -e "gpsd & gpsd.socket started\n"
+
+###################################################################################
+                                #Main repo installer
+								#Set up easy github install and file execute
+echo "###################################"
+echo "###### Github EZ Installer ########"
+echo "###################################"
+
+#get the repo we want to add to the rc.local file
+echo "Use default project repo? (y/n/c)"
+if yes(); then
+    echo "using https://github.com/UTS-Team-404/Main_Project_Repo.git"
+    githuburl="https://github.com/UTS-Team-404/Main_Project_Repo.git"
+else
+    echo -e '\nEnter github branch url' 
+    echo '(e.g. https://github.com/UTS-Team-404/Main_Project_Repo.git):'
+    read githuburl
+fi
+
+
+#grab the repo file name for later
+repo=$(echo $githuburl | awk 'BEGIN { FS = "/" } ; { print $NF }' | cut -d "." -f1)
+echo Repo name found: $repo
+
+echo "Use default start file? (y/n/c)"
+if yes(); then
+    echo "using init.sh"
+    startfile="init.sh"
+else
+    echo -e '\nWhat is the file to run on startup?'
+    echo "(e.g. init.sh)"
+    read startfile
+fi
+
+#Read file to be executed incase its different
+
+#Decide where to clone the repo. Currently must be done manually outside of /
+echo "Clone the reop into $targetdir?"
+if yes(); then
+    sudo git -c $targetdir clone $githuburl
+    echo "Making all executable at /$targetdir/$repo/*"
+    sudo chmod +x /$targetdir/$repo/*
+else
+    echo "Clone the repo and add to rc.local manually eg. /path/to/repo/executable.file"
+    exit
+fi
+
+###################################################################################
+                                #Setup rc.local
+								#(Wipe and) Set up rc.local
+echo "###################################"
+echo "#### Setup rc.local autostart #####"
+echo "###################################"
+
+#Temp solution to stop rerunning causing issues is to just wipte the rc.local file each time. Since its depreciated, there shouldnt be anything important in there anyway.
+echo "Wiping and repopulating rc.local..."
+sudo sh -c 'echo "#!/bin/bash" > /etc/rc.local'
+sudo sh -c 'echo "sleep 1" >> /etc/rc.local'
+
+#This will make an identifier for where our added lines go incase we need to delete or edit them.
+echo ""
+echo "###################################" >> $absolutetarget
+echo "#Team 404 WiFinder autostart webUI#" >> $absolutetarget
+echo "###################################" >> $absolutetarget
+
+#This will add a line that updates from the github branch each time the device turns on. We have decided not to implement this as currently we are not having the device connect to the internet, making it pointless. But could be added in the future?
+#echo -e '\n--> writing git -c $targetdir clone $githuburl to $absolutetarget'
+#echo "git -c $targetdir clone $githuburl" >> $absolutetarget
+
+#This will add a line that runs the file on startup
+sudo echo "--> writing $targetdir$repo/$startfile to $absolutetarget"
+startfiledir=$(echo "/$targetdir/$repo/$startfile")
+sudo echo ${startfiledir// /} >> $absolutetarget
+        sudo sh -c 'echo "sleep 1" >> /etc/rc.local'
+
+
+
+
+
+###################################################################################
+                                #Wireless Setup
+								#Wireless Warning
+echo "###################################"
+echo "######### Wireless Setup ##########"
+echo "###################################"
+echo "Should now have $startfile run on system boot. Now setting up wifi config."
+echo "you will lose internet access after this if you do not have a third wifi adapter"
 
 
 								#Set up Hotspot config
@@ -116,7 +375,7 @@ read -s -n 1 -t 10
 
 
 
-								#Set up NFTables redirects
+								#Set up hotspot NFTables redirects
 echo "###################################"
 echo "## Setup Hotspot NFTables Config ##"
 echo "###################################"
@@ -138,49 +397,17 @@ read -s -n 1 -t 10
 
 
 
-								#Set up easy github install and file execute
+
+								#Set up Wifi Adapter to perminent monitor mode
 echo "###################################"
-echo "###### Github EZ Installer ########"
+echo "## Setup Wifi Adapter Mon Config ##"
 echo "###################################"
 
-#get which repo we want to add to the rc.local file
-echo -e '\nEnter github branch url' 
-echo '(e.g. https://github.com/UTS-Team-404/Init-Script-Prototype.git):'
-read githuburl
+#
 
-#grab the repo file name for later
-repo=$(echo $githuburl | awk 'BEGIN { FS = "/" } ; { print $NF }' | cut -d "." -f1)
-echo Repo name found: $repo
 
-#Read file to be executed incase its different
-echo -e '\nWhat is the file to run on startup?'
-read startfile
+echo -e '\nCheck that the above is correct'
+echo "Press any key to continue (or 10s)"
+read -s -n 1 -t 10
 
-#Decide where to clone the repo. Currently must be done manually outside of /
-echo "Clone the reop into $targetdir?"
-if yes(); then
-        git -c $targetdir clone $githuburl
-        echo "Making all executable at /$targetdir/$repo/*"
-        sudo chmod +x /$targetdir/$repo/*
-    else
-        echo "Clone the repo and add to rc.local manually eg. /path/to/repo/executable.file"
-        exit
-    fi
 
-#This will make an identifier for where our added lines go incase we need to delete them manually (or with the wiper eventually).
-echo ""
-echo "###################################" >> $absolutetarget
-echo "#Team 404 Wifi Analitics tool Init#" >> $absolutetarget
-echo "###################################" >> $absolutetarget
-
-#This will add a line that updates from the github branch each time the device turns on. We have decided not to implement this as currently we are not having the device connect to the internet, making it pointless. But could be added in the future?
-
-#echo -e '\n--> writing git -c $targetdir clone $githuburl to $absolutetarget'
-#echo "git -c $targetdir clone $githuburl" >> $absolutetarget
-
-#This will add a line that runs the file on startup
-echo "--> writing $targetdir$repo/$startfile to $absolutetarget"
-startfiledir=$(echo "/$targetdir/$repo/$startfile")
-echo ${startfiledir// /} >> $absolutetarget
-
-echo "Should not have $startfile run on system boot as well as device hotspot"
